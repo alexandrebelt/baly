01992b6386046c86093dc4c8183035e1edd0311a
Alexandre Beltramini <alexandre.belt@hotmail.com>
2025-11-06 16:34:44 -0300

    06-11-2025

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
-module.exports = {
-  publicPath: '/baly-cereja/'
-};
+//module.exports = {
+//  publicPath: '/baly-cereja/'
+//};
